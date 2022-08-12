aafa9e07d77c916bb901001410c7c42d704a18e3
devdatovo <dev.gustavodatovo@outlook.com>
2022-08-12 19:56:16 -0300

    Realizando o exercicio 05

---
+++ Exercicios/Exercicio5.cs
@@ -0,0 +1,23 @@
+﻿namespace ExerciciosTarget.Exercicios
+{
+    internal class Exercicio5
+    {
+        public string InverterCaracteres()
+        {
+            string texto = "Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;";
+            string textoInvertido = "";
+            List<char> caracteres = new();
+            foreach (var caracter in texto)
+            {
+                caracteres.Add(caracter);
+            }
+
+            for (int i = caracteres.Count; i > 0; i--)
+            {
+                textoInvertido += caracteres[i - 1];
+            }
+
+            return $"Texto original: \n{texto}\n\nTexto invertido: \n{textoInvertido}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using ExerciciosTarget.Exercicios;
+using System.Text.Json;
 
 Exercicio2 exercicio2 = new();
-Console.WriteLine(exercicio2.VerificaNumeroEmFibonacci());
+// Console.WriteLine(exercicio2.VerificaNumeroEmFibonacci());
+
+Exercicio5 exercicio5 = new();
+Console.WriteLine(exercicio5.InverterCaracteres());
 
 Console.ReadKey();
