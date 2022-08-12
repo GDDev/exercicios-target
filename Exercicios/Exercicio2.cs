2969f72daee42c294f3c0a1836782402a6165dda
devdatovo <dev.gustavodatovo@outlook.com>
2022-08-12 17:47:29 -0300

    Resolucao do exercicio 02

---
+++ Exercicios/Exercicio2.cs
@@ -0,0 +1,26 @@
+﻿namespace ExerciciosTarget.Exercicios
+{
+    internal class Exercicio2
+    {
+        public string VerificaNumeroEmFibonacci()
+        {
+            int anterior1 = 0, anterior2 = 1, fibonacci;
+            bool existeNumero = false;
+            Console.Write("Digite um número para procurar dentro da sequência de Fibonacci: ");
+            int.TryParse(Console.ReadLine(), out int numero);
+
+            if ((numero == 0) || (numero == 1)){
+                return $"O valor {numero} é um dos valores iniciais";
+            }
+
+            for (int i = 0; i <= numero; i++)
+            {
+                fibonacci = anterior1 + anterior2;
+                if (fibonacci == numero) existeNumero = true;
+                anterior1 = anterior2;
+                anterior2 = fibonacci;
+            }
+            return existeNumero ? $"O número informado {numero} pertence a sequência de Fibonacci!" : $"O número informado {numero} não pertence a sequência de Fibonacci!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ExerciciosTarget.Exercicios;
+
+Exercicio2 exercicio2 = new();
+Console.WriteLine(exercicio2.VerificaNumeroEmFibonacci());
+
+Console.ReadKey();
