59805e88b1f4f4b7fc5b05568455907d391d1c4d
devdatovo <dev.gustavodatovo@outlook.com>
2022-08-13 00:19:44 -0300

    Realizando o exercicio 03

---
+++ Exercicios/Exercicio3.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+
+namespace ExerciciosTarget.Exercicios
+{
+    internal class Exercicio3
+    {
+        private Faturamento[] InterpretarJson()
+        {
+            string arquivoJson = Path.GetFullPath("dados.json");
+            arquivoJson = arquivoJson.Replace(@"\bin\Debug\net6.0", "");
+
+            string jsonString = File.ReadAllText(arquivoJson);
+
+            var faturamentos = JsonConvert.DeserializeObject<Faturamento[]>(jsonString);
+            return faturamentos;
+        }
+
+        public double MenorFaturamento()
+        {
+            Faturamento[] faturamentos = InterpretarJson();
+
+            double menorValor = 99999;
+            foreach (var faturamento in faturamentos)
+            {
+                if(faturamento.Valor < menorValor) // Adicione "&& faturamento.Valor > 0" para o menor valor entre os dias nos quais houveram ganhos
+                {
+                    menorValor = faturamento.Valor;
+                }
+            }
+
+            return menorValor;
+        }
+
+        public double MaiorFaturamento()
+        {
+            Faturamento[] faturamentos = InterpretarJson();
+
+            double maiorValor = 0;
+            foreach (var faturamento in faturamentos)
+            {
+                if (faturamento.Valor > maiorValor)
+                {
+                    maiorValor = faturamento.Valor;
+                }
+            }
+
+            return maiorValor;
+        }
+
+        public int QtdDiasSuperioresMedia()
+        {
+            Faturamento[] faturamentos = InterpretarJson();
+
+            double soma = 0;
+            int diasProd = 0;
+
+            foreach (var fat in faturamentos)
+            {
+                if(fat.Valor > 0)
+                {
+                    soma += fat.Valor;
+                    diasProd++;
+                }
+            }
+            int qtdDiasAcimaMedia = 0;
+            foreach (var fat in faturamentos)
+            {
+                if (fat.Valor > (soma / diasProd)) qtdDiasAcimaMedia++;
+            }
+
+            return qtdDiasAcimaMedia;
+        }
+    }
+
+    public class Faturamento
+    {
+        [JsonProperty("dia")]
+        public int Dia { get; set; }
+
+        [JsonProperty("valor")]
+        public double Valor { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,10 +3,15 @@
 Exercicio2 exercicio2 = new();
 // Console.WriteLine(exercicio2.VerificaNumeroEmFibonacci());
 
+Exercicio3 exercicio3 = new();
+Console.WriteLine($"Menor faturamento do mês: {exercicio3.MenorFaturamento():C}");
+Console.WriteLine($"Maior faturamento do mês: {exercicio3.MaiorFaturamento():C}");
+Console.WriteLine($"Quantidade de dias em que o faturamento superou a média mensal: {exercicio3.QtdDiasSuperioresMedia()}");
+
 Exercicio4 exercicio4 = new();
-exercicio4.PercentualPorEstado();
+// exercicio4.PercentualPorEstado();
 
 Exercicio5 exercicio5 = new();
-//Console.WriteLine(exercicio5.InverterCaracteres());
+// Console.WriteLine(exercicio5.InverterCaracteres());
 
 Console.ReadKey();
